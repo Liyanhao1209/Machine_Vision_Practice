4ca214f4bbd608296f8574fd9b250c98f1c10e1f
Liyanhao1209 <3054117680@qq.com>
2023-10-10 12:27:24 +0800

    merge conflict

--- Lec2/difficultFaceDetector.py
+++ Lec2/difficultFaceDetector.py
@@ -2,12 +2,14 @@ import cv2
 
 # 原图
 img = cv2.imread("pic/faces2.jpg")
+
 cv2.imshow("Original img", img)
 # 使用预训练摸型创建cascade分类器
 faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
 
-gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-cv2.imshow("Gray img", gray_img)
+# gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# cv2.imshow("Gray img", gray_img)
+
 
 # ret,thres = cv2.threshold(gray_img, 140, 255, cv2.THRESH_BINARY)
 # thres = cv2.adaptiveThreshold(gray_img,130,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,11,-10)
@@ -19,7 +21,7 @@ cv2.imshow("Gray img", gray_img)
 # uf = faceCascade.detectMultiScale(u_area,1.05,8);
 # faces = faceCascade.detectMultiScale(img, 1.1, 8)
 # faces[0:uf.shape[0],0:uf.shape[1]]=uf
-faces = faceCascade.detectMultiScale(img, 1.05, 4)
+faces = faceCascade.detectMultiScale(img, 1.01, 8)
 # 前后两次相继的扫描中搜索窗口的比例系数，默认为1.1 即每次搜索窗口扩大10%
 # 构成检测目标的相邻矩形的最小个数
 
