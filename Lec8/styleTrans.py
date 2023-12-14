530b5e4a53a7f5570b22818f48dd49b1729fd1d8
unknown <3054117680@qq.com>
2023-12-14 10:38:29 +0800

    Lec8:homework

--- Lec8/styleTrans.py
+++ Lec8/styleTrans.py
@@ -7,18 +7,33 @@ cv2.imshow('original', image)
 cv2.waitKey(0)
 # '''图片预处理'''
 H, W = image.shape[:2]  # 获取尺寸
-blod = cv2.dnn.blobFromImage(image, 1, (H, W), (0, 0, 0), swapRB=True, crop=False)
+blob1 = cv2.dnn.blobFromImage(image, 1, (H, W), (0, 0, 0), swapRB=False, crop=False)
+blob2 = cv2.dnn.blobFromImage(image, 1, (H, W), (0, 0, 0), swapRB=True, crop=False)
 
-#net = cv2.dnn.readNet('starry_night.t7')
-net = cv2.dnn.readNet('mosaic.t7')
+net1 = cv2.dnn.readNet('starry_night.t7')
+net2 = cv2.dnn.readNet('mosaic.t7')
 
-net.setInput(blod)
-out = net.forward()
-out_new = out.reshape(out.shape[1], out.shape[2], out.shape[3])  # 将输出进行加一化处理
-cv2.normalize(out_new, out_new, norm_type=cv2.NORM_MINMAX)
-# 通道转换
-result = out_new.transpose(1, 2, 0)
+index = 1
 
-cv2.imshow('result', result)
-cv2.waitKey(0)
+
+def netOutput(net, blob):
+    global index
+    net.setInput(blob)
+    out = net.forward()
+    out_new = out.reshape(out.shape[1], out.shape[2], out.shape[3])  # 将输出进行加一化处理
+    cv2.normalize(out_new, out_new, norm_type=cv2.NORM_MINMAX)
+    # 通道转换
+    result = out_new.transpose(1, 2, 0)
+    cv2.imshow('result' + str(index), result)
+    index += 1
+    cv2.waitKey(0)
+
+
+def iterNet(nets, blobs):
+    for net in nets:
+        for blob in blobs:
+            netOutput(net, blob)
+
+
+iterNet([net1, net2], [blob1, blob2])
 cv2.destroyAllWindows()
